06aeb8edc4c77824d0359f146b76937375b58ab7
Brennon Toomepuu <btoomepuu@gmail.com>
2022-07-19 19:44:32 +0000

    user is able to add entry, entries are stored in local storage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 function photoUpdate(event) {
-  const $newImg = document.getElementById('img-url').value;
+  var $newImg = document.getElementById('imgUrl').value;
   $entryImg.src = $newImg;
 }
-const $entryImg = document.querySelector('.entry-img');
-document.getElementById('img-url').addEventListener('input', photoUpdate);
+var $entryImg = document.querySelector('.entry-img');
+document.getElementById('imgUrl').addEventListener('input', photoUpdate);
+
+function submitEntry(event) {
+  event.preventDefault();
+  var $title = $entryForm.elements.title.value;
+  var $imgUrl = $entryForm.elements.imgUrl.value;
+  var $note = $entryForm.elements.note.value;
+  var newEntry = {
+    title: $title,
+    imgUrl: $imgUrl,
+    note: $note
+  };
+  newEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(newEntry);
+  $entryImg.src = '/images/placeholder-image-square.jpg';
+  document.getElementById('entry-form').reset();
+}
+var $entryForm = document.getElementById('entry-form');
+$entryForm.addEventListener('submit', submitEntry);
